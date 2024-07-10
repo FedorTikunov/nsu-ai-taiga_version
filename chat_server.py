ff59064ab3008bae6d0303f806f8e432396fbced
Alex1um <ye4h@pm.me>
2024-07-10 14:53:04 +0700

    added flask debug option

--- chat_server.py
+++ chat_server.py
@@ -121,5 +121,5 @@ def send():
         return e
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=config.flask_debug)
     # app.run(host='localhost', port=5000)
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ token = '<ваш токен>' # получить у BotFather
 weights_path = "/userspace/dra/nsu-ai/team_code/models"
 llava_weights = "/userspace/viv/nsu-ai-taiga_version/Llava/llava_weight"
 debug = False
+flask_debug = True
