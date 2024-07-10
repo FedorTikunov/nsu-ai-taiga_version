cab3ed0b307b690a932550847c2f795690f14229
Alex1um <ye4h@pm.me>
2024-07-10 13:15:22 +0700

    updated config path

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 token = '<ваш токен>' # получить у BotFather
 weights_path = "/userspace/dra/nsu-ai/team_code/models"
-llava_weights = "/userspace/tfv/nsu-ai-taiga_version/llava_weight"
+llava_weights = "/userspace/viv/nsu-ai-taiga_version/Llava/llava_weight"
 debug = False
