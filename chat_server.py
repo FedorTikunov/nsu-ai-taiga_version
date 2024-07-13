0ff7b0b9720aaeaa9c5b16d1899dacbe5ea84768
Alex1um <ye4h@pm.me>
2024-07-13 16:54:01 +0700

    added param type for debug

--- chat_server.py
+++ chat_server.py
@@ -155,7 +155,8 @@ def set_param():
 @app.route("/get_param", methods=['POST'])
 def get_param():
     if "param" in request.form and request.form["param"] in config.__dict__:
-        return str(config.__dict__[request.form['param']])
+        param = config.__dict__[request.form['param']]
+        return f"{type(param)} = {param}"
     return "Неизвестный параметр"
 
 
