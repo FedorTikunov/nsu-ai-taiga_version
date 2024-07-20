48d2f369f768d96a63429520b3b06126d356f6f7
Alex1um <ye4h@pm.me>
2024-07-20 16:28:54 +0700

    data preprocess bug fix

--- team_code/dataset_preprocess.py
+++ team_code/dataset_preprocess.py
@@ -1,10 +1,13 @@
 import json
 import sys
 import os 
+from pathlib import Path
 from generate import generate_full_prompt, setup_model_and_tokenizer
 
 
-def main(dir_dataset, dir_path):    
+def main(dir_dataset: str, dir_path: str):
+    dir_dataset: Path = Path(dir_dataset)
+    dir_path: Path = Path(dir_path)
 
     model, processor = setup_model_and_tokenizer()
 
@@ -13,15 +16,24 @@ def main(dir_dataset, dir_path):
         data = json.load(f)
 
     for item in data:
-        item['image'] = os.path.join(dir_path, item['image'])
+        item['image'] = str(dir_path / item['image'])
 
     for item in data:
-        cur_query_list = [{'text': item['conversations'][0]['value'], 'image': item['image']}]
+        cur_query_list = [
+            {
+                "type": "image",
+                "content": item["image"],
+            },
+            {
+                "type": "text",
+                "content": item['conversations'][0]['value'],
+            }
+            ]
         
         item['conversations'][0]['value'], _ = generate_full_prompt(model, cur_query_list, ('', ''))
 
 
-    with open('your_file.json', 'w') as f:
+    with open(dir_dataset, 'w') as f:
         json.dump(data, f, indent=4)
 
 
